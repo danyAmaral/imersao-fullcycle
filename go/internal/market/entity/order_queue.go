b2797a25b51426dfd4f7ddf84587450453580a5c
danyAmaral <danyamaral27@gmail.com>
2023-06-21 19:19:44 -0300

    teste

--- go/internal/market/entity/order_queue.go
+++ go/internal/market/entity/order_queue.go
@@ -4,28 +4,22 @@ type OrderQueue struct {
 	Orders []*Order
 }
 
-// Less - Comparar o preço entre duas ordens
 func (oq *OrderQueue) Less(i, j int) bool {
 	return oq.Orders[i].Price < oq.Orders[j].Price
 }
 
-// Swap - Inverte a ordem das ordens
 func (oq *OrderQueue) Swap(i, j int) {
 	oq.Orders[i], oq.Orders[j] = oq.Orders[j], oq.Orders[i]
 }
 
-// Len - Retorna o tamanho da fila de nossas ordens
 func (oq *OrderQueue) Len() int {
 	return len(oq.Orders)
 }
 
-// Push - Adicionar uma ordem na fila
-func (oq *OrderQueue) Push(x any) {
+func (oq *OrderQueue) Push(x interface{}) {
 	oq.Orders = append(oq.Orders, x.(*Order))
-
 }
 
-// Pop - Remove uma ordem
 func (oq *OrderQueue) Pop() interface{} {
 	old := oq.Orders
 	n := len(old)
